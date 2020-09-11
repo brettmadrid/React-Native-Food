f3183ff1af0421d248b229e10af4b58b12a4085b
brettmadrid <brettmadrid9@gmail.com>
2020-09-10 22:01:04 -0700

    ResultsList component built with custom hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,28 +1,33 @@
 import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
+import ResultsList from '../components/ResultsList'
 import SearchBar from '../components/SearchBar'
-import yelp from '../api/yelp'
+import useResults from '../hooks/useResults'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('')
-  const [results, setResults] = useState([])
+  const [searchApi, results, errorMessage] = useResults()
 
-  const searchApi = async () => {
-    const response = await yelp.get('./search', {
-      params: {
-        limit: 50,
-        term: term,
-        location: 'Salem, OR',
-      },
+  const filterResultsByPrice = price => {
+    // price === '$' || '$$' || '$$$'
+    return results.filter(result => {
+      return result.price === price
     })
-    setResults(response.data.businesses)
   }
 
   return (
     <View>
-      <SearchBar term={term} onTermChange={setTerm} onTermSubmit={searchApi} />
-      <Text>Search Screen</Text>
+      <SearchBar
+        term={term}
+        onTermChange={setTerm}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
+      <ResultsList results={filterResultsByPrice('$')} title='Cost Effective' />
+      <ResultsList results={filterResultsByPrice('$$')} title='Bit Pricier' />
+      <ResultsList results={filterResultsByPrice('$$$')} title='Big Spender' />
+      <ResultsList results={filterResultsByPrice('$$$$')} title='Top Shelf' />
     </View>
   )
 }
